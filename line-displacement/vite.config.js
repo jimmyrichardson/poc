5ea635b694e96d1d3ae71fd5a328256bfe01c2b9
Jimmy <jlrichii@gmail.com>
2025-05-15 19:46:42 -0500

    Subdirectory build take 1

--- line-displacement/vite.config.js
+++ line-displacement/vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/poc/line-displacement/dist/',
   root: './line-displacement/src',
   build: {
     outDir: './../dist',
     rollupOptions: {
-      input: './line-displacement/main.js'
+      input: './line-displacement/main.js',
+      output: {
+        entryFileNames: 'assets/main.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/main.css';
+          }
+          return 'assets/[name][extname]';
+        }
+      }
     }
   },
 });
